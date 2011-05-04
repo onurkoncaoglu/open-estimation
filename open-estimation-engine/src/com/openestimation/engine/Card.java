43b074a5e0efbb9eccf61d715e5371f1b89297c6
mohamed.elkammar <mohamed.elkammar@gmail.com>
2011-05-04 02:50:17 +0000

    - Added content of src directory. - Added contents of com.openestimation.engine package. - Added Card.java - Added CardDeck.java - Added Rank.java - Added Suit.java - Added Trick.java - Added Trump.java

---
+++ open-estimation-engine/src/com/openestimation/engine/Card.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.openestimation.engine;
+
+/**
+ * @author mohamed
+ * 
+ * This class represents a card that
+ * has a suit and a rank.
+ */
+public class Card implements Suit, Rank{
+	public int suit;
+	public int rank;
+}
---
+++ open-estimation-engine/src/com/openestimation/engine/Trump.java
@@ -0,0 +1,14 @@
+package com.openestimation.engine;
+
+/**
+ * @author mohamed
+ *
+ * This interface defines possible trumps
+ */
+public interface Trump {
+	int NO_TRUMP = 0;
+	int SPADES = 1;
+	int HEARTS = 2;
+	int DIAMONDS = 3;
+	int CLUBS = 4;
+}
