8ddf85afd1543837f94d7a7eeabdcd7a10e86de9
mohamed.elkammar <mohamed.elkammar@gmail.com>
2011-05-04 04:16:02 +0000

    - Changed Trump interface to enum. I need the toString() method. - Modified comments on Trump and Suit.

--- open-estimation-engine/src/com/openestimation/engine/Trump.java
+++ open-estimation-engine/src/com/openestimation/engine/Trump.java
@@ -3,12 +3,12 @@ package com.openestimation.engine;
 /**
  * @author mohamed
  *
- * This interface defines possible trumps
+ * This enum defines possible trump suits
  */
-public interface Trump {
-	int NO_TRUMP = 0;
-	int SPADES = 1;
-	int HEARTS = 2;
-	int DIAMONDS = 3;
-	int CLUBS = 4;
+public enum Trump {
+	NO_TRUMP,
+	SPADES,
+	HEARTS,
+	DIAMONDS,
+	CLUBS;
 }
