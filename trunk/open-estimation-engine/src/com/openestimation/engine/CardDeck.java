435d4cabf3d76019b3aca56f229ca8bdca95cdf5
mohamed.elkammar@gmail.com <mohamed.elkammar@gmail.com@11ed788e-d3c3-11de-901b-a378632c6355>
2011-05-05 05:12:06 +0000

    - Reordered Suit entries. - Removed CardDeck.shuffle(). Shuffle operation will be done at CardDeck.deal(). - Added Card.Deck.initialize() to initialize deck with ordered 52 cards.

--- trunk/open-estimation-engine/src/com/openestimation/engine/CardDeck.java
+++ trunk/open-estimation-engine/src/com/openestimation/engine/CardDeck.java
@@ -11,25 +11,58 @@ import java.util.Vector;
  * Deck will be completely used after dealing to four players.
  * Each 'Player' will 'take' 13 'Cards'.
  */
-public class CardDeck {
+public class CardDeck implements Rank, Suit{
 	Vector<Card> deck;
 	
-	/**
-	 * Shuffles the deck.
-	 * This functions resets the deck. Be careful.
-	 */
-	public void shuffle(){
-		// TODO Implement
-		// TODO handle exceptions
-		deck.clear();
-		
-		//for (int i = Rank.ACE_LOW.get; i < array.length; i++) {
-			
-		//}
+	boolean initialized;
+	public boolean isInitialized() {
+		return initialized;
 	}
+
+
+	public CardDeck() {
+		initialize();
+	}
+
+
+	public void initialize() {
+		deck.clear(); // TODO check for exceptions
+		// Initialize Spades
+		for (int rank = TWO; rank <= ACE_HIGH; rank++){
+			Card card = new Card(); // to be added to deck.
+			card.rank = rank;
+			card.suit = SPADES; // hard coded loop for spades
+			deck.add(card);
+		}
+		// Initialize Hearts
+		for (int rank = TWO; rank <= ACE_HIGH; rank++){
+			Card card = new Card(); // to be added to deck.
+			card.rank = rank;
+			card.suit = HEARTS; // hard coded loop for hearts
+			deck.add(card);
+		}
+		// Initialize Diamonds
+		for (int rank = TWO; rank <= ACE_HIGH; rank++){
+			Card card = new Card(); // to be added to deck.
+			card.rank = rank;
+			card.suit = DIAMONDS; // hard coded loop for diamonds
+			deck.add(card);
+		}
+		// Initialize Clubs
+		for (int rank = TWO; rank <= ACE_HIGH; rank++){
+			Card card = new Card(); // to be added to deck.
+			card.rank = rank;
+			card.suit = CLUBS; // hard coded loop for clubs
+			deck.add(card);
+		}
+		this.initialized = true;
+	}
+	
 	
 	public Card deal(int cards) {
 		// TODO Implement
+		// TODO shuffle while you deal.
+		this.initialized = false;
 		return null;
 	}
 }
