47fd7e6206011dfdc7091fd02e2d73ce12f772c2
mohamed.elkammar@gmail.com <mohamed.elkammar@gmail.com@11ed788e-d3c3-11de-901b-a378632c6355>
2011-05-11 06:49:36 +0000

    - Added PlayerDirection interface - Implemented Trick.getWinningCard() - Added constructor to Card class. - Added test method testTrick() to Test.java

---
+++ trunk/open-estimation-engine/src/com/openestimation/engine/PlayerDirection.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openestimation.engine;
+
+/**
+ *
+ * @author mohamed
+ */
+public interface PlayerDirection {
+    int SOUTH = 0;
+    int EAST = 1;
+    int NORTH = 2;
+    int WEST = 3;
+}
