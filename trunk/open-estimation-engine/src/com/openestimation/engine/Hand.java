fb1acc7bec3958bacab6a2e0c74d2bdca704a868
mohamed.elkammar@gmail.com <mohamed.elkammar@gmail.com@11ed788e-d3c3-11de-901b-a378632c6355>
2011-05-13 04:35:43 +0000

    - Added PlayerBase.java Class. Needs implementation. - Added Hand.java Class. Needs implementation.

---
+++ trunk/open-estimation-engine/src/com/openestimation/engine/Hand.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openestimation.engine;
+
+/**
+ * This class provides a vector (ArrayList really) of Cards that are held
+ * by a Player.
+ * A Player can Sort Cards.
+ * A Player can Retrieve a certain Suit from Hand.
+ * A Player can Mark a Suit as TRUMP.
+ * A Player can Play a Card (removing it from Hand). 
+ * A player can Insert card back in Hand if not Valid (rejected by RuleSet).
+ * 
+ * @author mohamed
+ */
+public class Hand {
+    // TODO: Implement Hand Class.
+}
---
+++ trunk/open-estimation-engine/src/com/openestimation/engine/PlayerBase.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openestimation.engine;
+
+/**
+ * Base class for Final Player class.
+ * This class provides a Hand object of Cards that the Player will use
+ * to place Bids, play Rounds, Win and Lose Tricks.
+ * A Player will be able to view scores.
+ * @author mohamed
+ */
+public class PlayerBase {
+    // TODO: Implement PlayerBase Class.    
+}
