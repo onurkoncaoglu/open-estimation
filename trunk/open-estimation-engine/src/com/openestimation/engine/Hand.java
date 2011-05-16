2da5e77c8f5f7fc154b74fe7613b80bb235a0cde
mohamed.elkammar@gmail.com <mohamed.elkammar@gmail.com@11ed788e-d3c3-11de-901b-a378632c6355>
2011-05-16 22:30:45 +0000

    - Added: Hand.sortHand(). Not yet implemented. - Added: Hand.drawCard(Card card). - Added: Hand.drawCard(int rank, int suit). - Added: Hand.undoDraw(). - Added: Hand.isSuitExisit(int suit). - Added: Hand.getCardCountForSuit(int suit). - Added: Hand.getHighestRankInSuit(int suit). - Added: Hand.getAllCardsInSuit(int suit).

--- trunk/open-estimation-engine/src/com/openestimation/engine/Hand.java
+++ trunk/open-estimation-engine/src/com/openestimation/engine/Hand.java
@@ -4,17 +4,122 @@
  */
 package com.openestimation.engine;
 
+import java.util.ArrayList;
+
 /**
  * This class provides a vector (ArrayList really) of Cards that are held
  * by a Player.
  * A Player can Sort Cards.
  * A Player can Retrieve a certain Suit from Hand.
- * A Player can Mark a Suit as TRUMP.
  * A Player can Play a Card (removing it from Hand). 
  * A player can Insert card back in Hand if not Valid (rejected by RuleSet).
  * 
  * @author mohamed
  */
-public class Hand {
-    // TODO: Implement Hand Class.
+public class Hand implements Suit, Rank{
+    ArrayList<Card> hand;
+    static final int maximumCards = 13;
+    Card undoCard;
+    
+    public void sortHand(){
+        // TODO: Implement sortHand()
+    }
+    
+    public Card drawCard(Card card){
+        if (hand.contains(card)){
+            hand.remove(card); // remove card from hand.
+            undoCard = card; // Store removed card in case user wants to undo.
+            return card; // returns the card that was found in hand.
+        }
+        return null;
+    }
+    
+    /**
+     * Draws a card from hand (actually removing it).
+     * @param suit
+     * @param rank
+     * @return 
+     */
+    public Card drawCard(int suit, int rank){
+        Card card = new Card(suit, rank);
+        return card;
+    }
+    
+    /**
+     * Undos last draw from hand. Useful if card does not validate and user
+     * wants to undo the draw.
+     */
+    public void undoDraw(){
+        hand.add(undoCard);
+        this.sortHand();
+        undoCard = null;
+    }
+    
+    /**
+     * Checks of a certain suit exists in a Hand.
+     * @param suit: The suit to check for.
+     * @return Returns true if it found at least on card of the
+     * specified suit. Returns false otherwise.
+     */
+    public boolean isSuitExist(int suit){
+        for (Card card : hand) {
+            if (card.getSuit() == suit)
+                return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Gets card count in a suit.
+     * @param suit: The suit to count.
+     * @return card count. Returns 0 if none exist.
+     */
+    public int getCardCountForSuit(int suit){
+        int cardCounter = 0;
+        for (Card card : hand){
+            if (card.getSuit() == suit)
+                cardCounter++;
+        }
+        return cardCounter;
+    }
+    
+    /**
+     * Gets the highest Rank for a specified Suit in Hand.
+     * User should check first if suit exists. Otherwise, user will get -1 for
+     * non-existing suit.
+     * @param suit: The suit to get the highest card from.
+     * @return Highest rank returned. If suit does not exist, -1 is returned.
+     */
+    public int getHighestRankInSuit(int suit) {
+        Card highCard = new Card();
+        highCard.setRank(TWO);
+        int rank = -1;
+        for (Card card : hand){
+            if (card.suit == suit){
+                if (card.rank >= highCard.rank){
+                    highCard = card;
+                    rank = highCard.getRank();
+                }
+            }
+        }
+        return rank;
+    }
+    
+    /**
+     * Returns an ArrayList containing all cards in a suit.
+     * @param suit
+     * @return 
+     */
+    public ArrayList<Card> getAllCardsInSuit(int suit){
+        ArrayList<Card> list = new ArrayList<Card>();
+        for (Card card : hand){
+            if (card.suit == suit){
+                list.add(card);
+            }
+        }
+        if (list.size() > 0)
+            return list;
+        else
+            return null;
+    }
 }
--- trunk/open-estimation-engine/src/com/openestimation/engine/PlayerBase.java
+++ trunk/open-estimation-engine/src/com/openestimation/engine/PlayerBase.java
@@ -5,12 +5,14 @@
 package com.openestimation.engine;
 
 /**
- * Base class for Final Player class.
+ * This is a base class for the Player class.
+ * This will provide methods to override by children AI players.
+ * One special child class is the human Player.
  * This class provides a Hand object of Cards that the Player will use
  * to place Bids, play Rounds, Win and Lose Tricks.
  * A Player will be able to view scores.
  * @author mohamed
  */
 public class PlayerBase {
-    // TODO: Implement PlayerBase Class.    
+    // TODO: Implement PlayerBase Class.
 }
