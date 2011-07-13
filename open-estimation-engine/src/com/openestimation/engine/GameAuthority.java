d49c53e1350bc3d305518a7a2b1cfd20db0ab3d3
mohamed.elkammar <mohamed.elkammar@gmail.com>
2011-07-13 16:47:03 +0000

    - Added GameAuthority.java class, previously known as RuleSet. - This class shall handle the execution of the Open Estimation game.

---
+++ open-estimation-engine/src/com/openestimation/engine/GameAuthority.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openestimation.engine;
+
+/**
+ *
+ * @author mohamed
+ */
+public class GameAuthority {
+    
+}
