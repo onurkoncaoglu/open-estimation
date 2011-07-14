c85d7e015f611565f9e5bada257c67b87bde781d
mohamed.elkammar@gmail.com <mohamed.elkammar@gmail.com@11ed788e-d3c3-11de-901b-a378632c6355>
2011-07-14 03:06:06 +0000

    - Added some getters and setters to PlayerBase class. TODO more work here.

--- trunk/open-estimation-engine/src/com/openestimation/engine/PlayerBase.java
+++ trunk/open-estimation-engine/src/com/openestimation/engine/PlayerBase.java
@@ -11,8 +11,41 @@ package com.openestimation.engine;
  * This class provides a Hand object of Cards that the Player will use
  * to place Bids, play Rounds, Win and Lose Tricks.
  * A Player will be able to view scores.
+ * 
+ * TODO: Should we make this an abstract class?
+ * 
  * @author mohamed
  */
-public class PlayerBase {
-    // TODO: Implement PlayerBase Class.
+public abstract class PlayerBase {
+
+    public PlayerBase() {
+    }
+    
+    /**
+     * This method allows a Player to select a card
+     * from hand and return it to the RuleSet caller object. 
+     * That card must be validated prior to return. It is the player's
+     * responsibility to return a valid card.
+     * 
+     * @param trick
+     * @return 
+     */
+    abstract public Card play(Trick trick);
+    abstract public Bid placeBid();
+    
+    private int score;
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+    
+    private Hand hand;
+
+    public Hand getHand() {
+        return hand;
+    }
 }
